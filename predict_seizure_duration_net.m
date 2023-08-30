9d764b26f541fa834aff21559cb5e10ce6467fb8
yuzhangc <59620029+yuzhangc@users.noreply.github.com>
2023-08-29 23:25:25 -0700

    Beginning Work on Neural Networks

--- predict_seizure_duration_net.m
+++ predict_seizure_duration_net.m
@@ -12,19 +12,52 @@ function [seizure_duration,min_thresh,output_array,sz_parameters] = predict_seiz
 % Deep learning will be performed on concactenated arrays with manual
 % categorization of seizure or not.
 
+% -------------------------------------------------------------------------
+
+% Step 1: Read Features
+
+disp("Working on: " + path_extract)
+% Loads Features
+load(strcat(path_extract,'Normalized Features.mat'));
+sz_parameters = readmatrix(strcat(path_extract,'Trials Spreadsheet.csv'));
+
+% Extracts Feature Names
+feature_names = fieldnames(norm_features);
+
+% -------------------------------------------------------------------------
+
+% Step 0: Training Model
+
 % Input Size is Number Features
-inputSize = 12;
+inputSize = length(feature_names);
+
+if sum(ismember(feature_names,'Band_Power'))
+    inputSize = inputSize + size(bp_filters,1) - 1;
+end
+
+% Determine Size of Time Space and Channel Count
+
+if (isequal(feature_names{end},'Band_Power'))
+    total_timeseg = size(norm_features.(feature_names{end}){1}{training_segment},1);
+    total_ch = size(norm_features.(feature_names{end}){1}{training_segment},2);
+else
+    total_timeseg = size(norm_features.(feature_names{end}){training_segment},1);
+    total_ch = size(norm_features.(feature_names{end}){training_segment},2);
+end
+
+% Parameters Copied From Below Training Guide, Will Modify As Needed
+% https://www.mathworks.com/help/deeplearning/ref/trainnetwork.html
+
 numHiddenUnits = 100;
 numClasses = 2;
 
+% Deep Learning Layers
 layers = [ ...
-    sequenceInputLayer(inputSize)
+    sequenceInputLayer([1, inputSize, total_ch])
     lstmLayer(numHiddenUnits,'OutputMode','last')
     fullyConnectedLayer(numClasses)
     softmaxLayer
     classificationLayer];
-
-% https://www.mathworks.com/help/deeplearning/ref/trainnetwork.html#mw_36a68d96-8505-4b8d-b338-44e1efa9cc5e
 
 maxEpochs = 70;
 miniBatchSize = 27;
@@ -37,7 +70,55 @@ options = trainingOptions('adam', ...
     'Verbose',false, ...
     'Plots','training-progress');
 
+% SPECIFIC: Training Was Done With Animal 37, Trial 10.
+
+% Trial 10 is Location #9 in the Array
+training_segment = 9;
+
+% Create Zero Array For Population
+
+training_set = zeros(1,inputSize,total_ch,total_timeseg);
+
+for current_ch = 1:total_ch
+current_idx = 1;
+
+for feature = 1:length(feature_names)
+
+    % Special Case for Band Power
+    if (isequal(feature_names{feature},'Band_Power'))
+           for bp_cnt = 1:size(bp_filters,1)
+                
+               % Arranges into 1 Row By Timepoint By Channel By Trial
+               % Format
+               for trial = 1:total_timeseg 
+               training_set(1,current_idx,current_ch,trial) = norm_features.(feature_names{feature}){bp_cnt}{training_segment}(trial,current_ch);
+               end
+
+               current_idx = current_idx + 1;
+
+           end
+    else
+
+    % Otherwise, add features in correct channel array.
+
+    for trial = 1:total_timeseg 
+    training_set(1,current_idx,current_ch,trial) = norm_features.(feature_names{feature}){training_segment}(trial,current_ch);
+    end
+
+    current_idx = current_idx + 1;
+
+    end
+
+end
+
+end
+
+% Training 'Responses'
+manual_length = 60;
+training_outputs = zeros(total_timeseg,1);
+training_outputs(t_before/winDisp:(t_before + sz_parameters(training_segment,12) + manual_length)/winDisp) = 1;
+
 % Train the LSTM network with the specified training options.
-net = trainNetwork(XTrain,YTrain,layers,options);
+net = trainNetwork(training_set,training_outputs,layers,options);
 
 end
