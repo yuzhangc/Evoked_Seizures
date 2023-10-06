197a50e5f8641a7cca7e7707297b71dce6c8ad31
yuzhangc <59620029+yuzhangc@users.noreply.github.com>
2023-10-05 23:05:20 -0400

    Figure Out How to Reorganize Data in R

--- R/matlab_data_integration.R
+++ R/matlab_data_integration.R
@@ -1,7 +1,8 @@
-# Import Libraries
+# Step 1: Import Libraries and Generate List of SubFolders
 
 library(ggplot2)
 library(nlme)
+library(dplyr)
 
 # Change to local folder directory
 
@@ -14,9 +15,13 @@ real_folder_st <- match(paste(directory,"00000000 DO NOT PROCESS",sep = ""),comp
 real_folder_end <- match(paste(directory,"99999999 END",sep = ""),complete_list)
 subFolders <- complete_list[(real_folder_st + 1):(real_folder_end - 1)]
 
-# Select One Folder
+# ---------------------------------------------------------------------------------------------------
 
-folder_num <- 37
+# Step 2: Loops Through All Folder To Generate Complete Data Frame
+
+all_data <- data.frame()
+  
+for (folder_num in seq(1:length(subFolders))) {
 
 # Imports Files
 csv_file_list <- list.files(path = subFolders[folder_num], pattern = "Extracted_Features_Channel_V2",
@@ -27,19 +32,53 @@ target_ch <- 1
 
 # Reads CSV into Dataframe
 
-feature_data <- read.csv(paste(subFolders[folder_num],csv_file_list[2],sep="/"))
+feature_data <- read.csv(paste(subFolders[folder_num],csv_file_list[target_ch],sep="/"))
 
-# Make Time Point Factor
+# Converts Certain Variables Into Right Types
 
-feature_data$Time.Point <- factor(feature_data$Time.Point )
+feature_data$Successful.Evocation <- as.logical(feature_data$Successful.Evocation)
+feature_data$Epileptic <- as.logical(feature_data$Epileptic)
+feature_data$Diazepam <- as.logical(feature_data$Diazepam)
+feature_data$Levetiracetam <- as.logical(feature_data$Levetiracetam)
+feature_data$Phenytoin <- as.logical(feature_data$Phenytoin)
+
+# Make Time Point A Factor. All Other Factorization Will Happen Post-Op
+
+feature_data$Time.Point <- factor(feature_data$Time.Point)
+
+# Fixes Gender Imported As 'False'
+
+if (any(feature_data$Gender == 'FALSE')) feature_data$Gender[which(feature_data$Gender == 'FALSE')] <- 'F'
+
+# Replace -1 With NA
+
+feature_data$Laser.2...Color[which(feature_data$Laser.2...Color == -1)] <- NA
+feature_data$Laser.2...Power[which(feature_data$Laser.2...Power == -1)] <- NA
+feature_data$Laser.2...Duration[which(feature_data$Laser.1...Duration == -1)] <- NA
+feature_data$Laser.2...Frequency[which(feature_data$Laser.2...Frequency == -1)] <- NA
+feature_data$Delay[which(feature_data$Delay == -1)] <- NA
+
+# Append to All_Data
+
+all_data <- rbind(all_data,feature_data)
+
+}
+
+# ---------------------------------------------------------------------------------------------------
+
+# Step 3: Filters Data Frame
 
 # Remove Short Events
 
-min_time <- 15
+min_time <- readline(prompt="Do you want to exclude short events?\nIf so, type in the second duration of events to exclude.\nAny events smaller than the duration will be excluded: ")
+kept_indices <- which(all_data$Evoked.Activity.Duration > as.numeric(min_time))
+all_data <- all_data[kept_indices,]
 
-indices <- which(feature_data$Evoked.Activity.Duration > min_time)
+# Remove Early Recordings
 
-feature_data <- feature_data[indices,]
+min_anim <- readline(prompt="Do you want to exclude early animals?\nIf so, type in the smallest animal to exclude.\nAny events smaller than or equal to it will be excluded. \nCommon Ones: 12 = 2022/11/07, 22 = 2023/01/16: ")
+kept_indices <- which(all_data$Animal > as.integer(min_anim))
+all_data <- all_data[kept_indices,]
 
 # Responder - Band Power Predictor - Time Point Random Effect Epileptic?
 summary(lme(Ch.2.Band.Power.1.Hz.to.30Hz ~ Time.Point, random = ~ 1|Epileptic, data = feature_data))
