39341d4123579ca39007302ad175d755625a94a7
yuzhangc <59620029+yuzhangc@users.noreply.github.com>
2024-02-12 18:37:25 -0500

    Update extract_data_R_V2.m

--- extract_data_R_V2.m
+++ extract_data_R_V2.m
@@ -135,9 +135,29 @@ col17 = num2cell(sz_parameters(:,18));
 
 col18 = num2cell(seizure_duration_list{folder_num});
 
-% Colum 19 - Seizure Number
+% Column 19 - Seizure Number
 
 col19 = num2cell(sz_parameters(:,2));
+
+% For Freely Moving Animals Only
+
+if sz_parameters(1,1) > 99
+
+% Column 20 - Evocation Day
+
+col20 = num2cell(sz_parameters(:,20));
+
+% Column 21 - Behavioral Seizure Scale
+
+col21 = num2cell(sz_parameters(:,21));
+
+beginning_data = table(col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,...
+    col13,col14,col15,col16,col17,col18,col19,col20,col21,'VariableNames',["Animal","Epileptic","Gender","Age",...
+    "Weeks Post KA","Successful Evocation","Laser 1 - Color","Laser 1 - Power","Laser 1 - Duration",...
+    "Laser 2 - Color", "Laser 2 - Power", "Laser 2 - Duration","Delay","Laser 2 - Frequency",...
+    "Diazepam","Levetiracetam","Phenytoin","Evoked Activity Duration","Trial Number","Evocation Day","Seizure Scale"]);
+
+else
 
 % Create Tables For Common Data
 
@@ -146,6 +166,8 @@ beginning_data = table(col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,
     "Weeks Post KA","Successful Evocation","Laser 1 - Color","Laser 1 - Power","Laser 1 - Duration",...
     "Laser 2 - Color", "Laser 2 - Power", "Laser 2 - Duration","Delay","Laser 2 - Frequency",...
     "Diazepam","Levetiracetam","Phenytoin","Evoked Activity Duration","Trial Number"]);
+
+end
 
 % -------------------------------------------------------------------------
 
